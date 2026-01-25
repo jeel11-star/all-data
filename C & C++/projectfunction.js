35afa296094c8bb4bcca26460bfff835ea123f38
jeel sakhiya <sakhiyajeel11@gmail.com>
2026-01-25 19:05:28 +0530

    condition with looping programs

---
+++ C & C++/projectfunction.js
@@ -0,0 +1,71 @@
+
+
+
+// // ----------------Q-1-------------------------------------
+// console.log("enter the number of fib series needed");
+
+// function fib(n){
+// let n1 = 0,n2 = 1, nextTerm;
+
+//     for(let i=0;i<=n;i++)
+//         {
+//         console.log(n1);
+//         nextTerm = n1 +n2;
+//         n1=n2;
+//         n2=nextTerm;
+
+
+//     }
+
+// }
+
+// fib(5);
+
+// // --------------------Q-2----------------------------------------------------
+  
+// function printDigitSum(num) {
+//   let sum = 0;
+//   let digits = num.toString().split(""); // split number into digits
+
+//   for (let i = 0; i < digits.length; i++) {
+//     sum = sum + Number(digits[i]);
+//   }
+//   console.log(sum);
+// }
+// printDigitSum(389);
+// // -------------------------------------q-3------------------------------------
+
+// function Palindrome(value) {
+//   let str = value.toString();           
+//   let reversed = str.split("").reverse().join("");
+
+//   if (str === reversed) {
+//     console.log("Palindrome");
+//   } else {
+//     console.log("Not Palindrome");
+//   }
+// }
+
+// Palindrome(121);   
+// // ------------------------------q-4------------------------------------
+
+
+// function factorial(num) {
+//   let result = 1;
+
+//   for (let i = 1; i <= num; i++) {
+//     result = result * i;
+//   }
+
+//   return result;
+// }
+
+// console.log(factorial(5));
+// // -----------------------------------------q-5------------------------------
+// function isreverse(n){
+
+//     let arr1=[1,2,3,4,5,6,7,8,9];
+//     console.log(arr1.reverse());
+// }
+
+// isreverse();
